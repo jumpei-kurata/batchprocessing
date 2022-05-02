623b13a024c5b644a1fcc0966c0a03f1a12add4a
倉田順平 <>
2022-05-02 19:07:15 +0900

    no message

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,6 +1,7 @@
 DROP TABLE people IF EXISTS;
+DROP TABLE people IF EXISTS;
 
-CREATE TABLE people  (
+CREATE TABLE people (
     person_id BIGINT IDENTITY NOT NULL PRIMARY KEY,
     first_name VARCHAR(20),
     last_name VARCHAR(20)
